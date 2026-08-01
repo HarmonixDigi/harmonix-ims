121a56c5eaa8496a3be2aaaa9deb5dd92c0d9364
KrishVarsh <padmakshihk@gmail.com>
2026-08-01 08:43:19 +0530

    Fix: Harmonix logo for app icon, swan for favicon, bump SW cache to v2

--- harmonix-ims/public/sw.js
+++ harmonix-ims/public/sw.js
@@ -1,3 +1,3 @@
-const CACHE = 'harmonix-v1'
+const CACHE = 'harmonix-v2'
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/']))))
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))))
